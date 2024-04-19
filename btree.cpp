01616b18360b48f5c2e006b0a5a2edc563d47723
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-04-19 15:43:17 +0300

    Продолжение работы над insert

--- btree.cpp
+++ btree.cpp
@@ -22,11 +22,6 @@ public:
 		_parent = nullptr;
 	}
 
-	BTreeNode(vector<T>::iterator start, vector<T>::iterator end) {
-		_keys = vector<T>(start, end);
-		_children = vector<BTreeNode<T>*>(0);
-		_parent = nullptr;
-	}
 
 	bool contains(T key) {
 		for (const auto val : _keys) {
@@ -49,6 +44,43 @@ public:
 	BTree(int t) : _root(nullptr), _t(t) {};
 
 	//insert
+
+	BTreeNode<T>* help_find_parent(BTreeNode<T>* parent, BTreeNode<T>* to_find) {
+		for (auto val& : parent->_children) {
+			if (val == to_find) {
+				return parent;
+			}
+		}
+		return 
+	}
+
+	BTreeNode<T>* find_parent(BTreeNode<T>* node) {
+		return help_find_parent(_root, node);
+	}
+
+
+	BTreeNode<T>* half_node(BTreeNode<T>* p, char side) {
+		BTreeNode<T>* res = new BTreeNode<T>();
+		int start, end;
+		int child_size = p->_children.size();
+		if (side == 'l') {
+			start = 0;
+			end = _t - 1;
+
+			res->_children = vector<BTreeNode<T>*>(p->_children.begin(), p->_children.begin() + child_size / 2);
+		}
+		else if (side == 'r') {
+			start = _t;
+			end = 2 * _t - 1;
+
+			res->_children = vector<BTreeNode<T>*>(p->_children.begin() + child_size / 2, p->_children.end());
+		}
+		res->_keys = vector<T>(p->_keys.begin() + start, p->_keys.begin() + end);
+		res->_parent = p->_parent;
+		return res;
+	}
+
+
 	BTreeNode<T>* find_node_to_ins(BTreeNode<T>* root, T key) {
 		int child_size = root->_children.size();
 		if (child_size == 0) {
@@ -68,17 +100,39 @@ public:
 		return find_node_to_ins(root->_children[child_size - 1], key);
 	}
 
-	void split_node(BTreeNode<T>* node) {
+	void split_node(BTreeNode<T>*& node) {
 		BTreeNode<T>* parent = node->_parent;
 		if (!parent) {
-			BTreeNode<T>* new_root = new BTreeNode<T>(node->_keys.begin() + _t - 1, node->_keys.begin() + _t);
-			BTreeNode<T>* l_node = new BTreeNode<T>(node->_keys.begin(), node->_keys.begin() + _t - 1);
-			BTreeNode<T>* r_node = new BTreeNode<T>(node->_keys.begin() + _t, node->_keys.end());
+			BTreeNode<T>* new_root = new BTreeNode<T>();
+			new_root->_keys.push_back(node->_keys[_t - 1]);
+			BTreeNode<T>* l_node = half_node(node, 'l');
+			BTreeNode<T>* r_node = half_node(node, 'r');
 			new_root->_children.push_back(l_node);
 			new_root->_children.push_back(r_node);
 			l_node->_parent = new_root;
 			r_node->_parent = new_root;
 			_root = new_root;
+			node = new_root
+		}
+		else {
+			if (parent->_keys.size() == 2 * _t - 1) {
+				split_node(parent);
+				delete parent;
+			}
+			BTreeNode<T>* l_node = half_node(node, 'l');
+			BTreeNode<T>* r_node = half_node(node, 'r');
+			parent->_keys.push_back(node->_keys[_t - 1]);
+			sort(node->_keys.begin(), node->_keys.end());
+			parent->_children.push_back(l_node);
+			parent->_children.push_back(r_node);
+			l_node->_parent = parent;
+			r_node->_parent = parent;
+			for (int i = 0; i < parent->_children.size(); i++) {
+				if (parent->_children[i] == node) {
+					parent->_children.erase(parent->_children.begin() + i);
+					break;
+				}
+			}
 		}
 	}
 
@@ -100,6 +154,7 @@ public:
 			return true;
 		}
 		split_node(to_ins);
+		delete to_ins;
 		return this->insert(key);
 	}
 
@@ -119,6 +174,7 @@ public:
 
 	void print() {
 		help_print(_root);
+		cout << endl;
 	}
 
 	//print end
--- tree_tests.cpp
+++ tree_tests.cpp
@@ -4,20 +4,10 @@
 
 using namespace std;
 
-TEST(BTreeTests, NodeByIterCheck) {
-	vector<int> v;
-	for (int i = 0; i < 6; i++) {
-		v.push_back(i);
-	}
-	BTreeNode<int> a(v.begin(), v.begin() + 3);
-}
-
 TEST(BTreeTests, InsertCheck) {
 	BTree<int> a(2);
-	a.insert(2);
-	a.insert(1);
-	a.insert(3);
-	a.insert(0);
-	a.insert(0);
-	a.print();
+	for (int i = 0; i < 10; i++) {
+		a.insert(i);
+		a.print();
+	}
 }
