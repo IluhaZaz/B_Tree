8afed834dc33c33b974b06ed1d4210ef99ce2787
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-06-08 11:21:12 +0300

    Автоматизация для постройки графиков

---
+++ bin_tree.cpp
@@ -0,0 +1,148 @@
+#pragma once
+#include<iostream>
+#include <random>
+#include <string>
+#include <chrono>
+#include <cstdint>
+
+using namespace std;
+
+template<typename T>
+class Node {
+public:
+    T data;
+    Node<T>* left;
+    Node<T>* right;
+    Node(T value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+template<typename T>
+class BinaryTree {
+public:
+    BinaryTree() {
+        _root = nullptr;
+    }
+
+    ~BinaryTree() {
+        clear(_root);
+    }
+
+    void print() {
+        print(_root);
+        std::cout << std::endl;
+    }
+
+    bool insert(T value) {
+        return insert(_root, value);
+    }
+
+    bool contains(T value) {
+        return contains(_root, value);
+    }
+
+    bool erase(T value) {
+        return erase(_root, value);
+    }
+
+    Node<T>* get_root() const {
+        return _root;
+    }
+
+private:
+    Node<T>* _root;
+
+    Node<T>* copy(Node<T>* node) {
+        if (node) {
+            Node<T>* new_node = new Node<T>(node->data);
+            new_node->left = copy(node->left);
+            new_node->right = copy(node->right);
+            return new_node;
+        }
+        return nullptr;
+    }
+
+    bool insert(Node<T>*& node, T value) {
+
+        if (contains(value))
+            return false;
+
+        if (node == nullptr) {
+            node = new Node<T>(value);
+            return true;
+        }
+        if (value < node->data) {
+            return insert(node->left, value);
+        }
+        else if (value > node->data) {
+            return insert(node->right, value);
+        }
+        return false;
+    }
+
+    void print(Node<T>* node) {
+        if (node) {
+            print(node->left);
+            std::cout << node->data << " ";
+            print(node->right);
+        }
+    }
+
+    void clear(Node<T>* node) {
+        if (node) {
+            clear(node->left);
+            clear(node->right);
+            delete node;
+        }
+    }
+
+
+    bool contains(Node<T>* node, T value) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (value < node->data) {
+            return contains(node->left, value);
+        }
+        else if (value > node->data) {
+            return contains(node->right, value);
+        }
+        return true;
+    }
+
+    bool erase(Node<T>*& node, T value) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (value < node->data) {
+            return erase(node->left, value);
+        }
+        else if (value > node->data) {
+            return erase(node->right, value);
+        }
+        else {
+            if (node->left == nullptr) {
+                Node<T>* temp = node->right;
+                delete node;
+                node = temp;
+            }
+            else if (node->right == nullptr) {
+                Node<T>* temp = node->left;
+                delete node;
+                node = temp;
+            }
+            else {
+                Node<T>* temp = node->right;
+                while (temp->left) {
+                    temp = temp->left;
+                }
+                node->data = temp->data;
+                erase(node->right, temp->data);
+            }
+            return true;
+        }
+    }
+};
--- testing.cpp
+++ testing.cpp
@@ -3,88 +3,100 @@
 #include <vector>
 #include <random>
 #include <chrono>
-#include "set.cpp"
-#include <gtest/gtest.h>
+#include "bin_tree.cpp"
 #include <algorithm>
+#include <fstream>
 
 
-TEST(Tests, InsertCheck) {
+void main() {
+	{
+		vector<int> sizes{ 10, 250, 500, 1000, 2000, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 35000, 70000 };
 
-	vector<int> sizes{ 10, 250, 500, 1000, 2000, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 35000 };
+		std::ofstream file;
+		file.open("C:\\b_tree\\insert.txt");
 
-	for (const auto& size : sizes) {
+		for (const auto& size : sizes) {
 
+			BTree<int> b(30);
+			auto begin = std::chrono::steady_clock::now();
+			for (int i = 0; i < size; i++) {
+				b.insert(rand());
+			}
+			auto end = std::chrono::steady_clock::now();
+			auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+			std::cout << elapsed_ms << " BTree for " << size << " values" << endl;
+			file << elapsed_ms.count() << " ";
 
-		BTree<int> b(30);
-		auto begin = std::chrono::steady_clock::now();
-		for (int i = 0; i < size; i++) {
-			b.insert(rand());
+			BinaryTree<int> s;
+			begin = std::chrono::steady_clock::now();
+			for (int i = 0; i < size; i++) {
+				s.insert(rand());
+			}
+			end = std::chrono::steady_clock::now();
+			elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+			std::cout << elapsed_ms << " Set for " << size << " values" << endl;
+			file << elapsed_ms.count() << " ";
+
+			vector<int> v;
+			begin = std::chrono::steady_clock::now();
+			for (int i = 0; i < size; i++) {
+				v.push_back(rand());
+			}
+			end = std::chrono::steady_clock::now();
+			elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+			std::cout << elapsed_ms << " vector for " << size << " values" << endl;
+			file << elapsed_ms.count() << " ";
 		}
-		auto end = std::chrono::steady_clock::now();
-		auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-		std::cout << elapsed_ms << " BTree" << endl;
-
-		Set s;
-		begin = std::chrono::steady_clock::now();
-		for (int i = 0; i < size; i++) {
-			s.insert(rand());
-		}
-		end = std::chrono::steady_clock::now();
-		elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-		std::cout << elapsed_ms << " Set" << endl;
-
-		vector<int> v;
-		begin = std::chrono::steady_clock::now();
-		for (int i = 0; i < size; i++) {
-			v.push_back(rand());
-		}
-		end = std::chrono::steady_clock::now();
-		elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-		std::cout << elapsed_ms << " vector" << endl;
+		file.close();
 	}
-}
 
-TEST(Tests, FindCheck) {
+	{
+		std::ofstream file;
+		file.open("C:\\b_tree\\search.txt");
 
-	vector<int> sizes{ 10, 250, 500, 1000, 2000, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 35000 };
-	for (const auto& size : sizes) {
+		vector<int> sizes{ 10, 250, 500, 1000, 2000, 5000, 7500, 10000, 15000, 20000, 25000, 30000, 35000 };
+		for (const auto& size : sizes) {
 
-		BTree<int> b(30);
-		Set s;
-		vector<int> v;
+			BTree<int> b(30);
+			BinaryTree<int> s;
+			vector<int> v;
 
-		vector<int> values;
-		for (int i = 0; i < size; i++) {
-			int r = rand();
-			b.insert(r);
-			s.insert(r);
-			v.push_back(r);
+			vector<int> values;
+			for (int i = 0; i < size; i++) {
+				int r = rand();
+				b.insert(r);
+				s.insert(r);
+				v.push_back(r);
 
-			values.push_back(r);
+				values.push_back(r);
+			}
+
+			auto begin = std::chrono::steady_clock::now();
+			for (int i = 0; i < size; i++) {
+				b.find(values[i]);
+			}
+			auto end = std::chrono::steady_clock::now();
+			auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+			std::cout << elapsed_ms << " BTree for " << size << " values" << endl;
+			file << elapsed_ms.count() << " ";
+
+			begin = std::chrono::steady_clock::now();
+			for (int i = 0; i < size; i++) {
+				s.contains(values[i]);
+			}
+			end = std::chrono::steady_clock::now();
+			elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+			std::cout << elapsed_ms << " Set for " << size << " values" << endl;
+			file << elapsed_ms.count() << " ";
+
+			begin = std::chrono::steady_clock::now();
+			for (int i = 0; i < size; i++) {
+				std::find(v.begin(), v.end(), values[i]);
+			}
+			end = std::chrono::steady_clock::now();
+			elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+			std::cout << elapsed_ms << " Vector for " << size << " values" << endl;
+			file << elapsed_ms.count() << " ";
 		}
-
-		auto begin = std::chrono::steady_clock::now();
-		for (int i = 0; i < size; i++) {
-			EXPECT_TRUE(b.find(values[i]));
-		}
-		auto end = std::chrono::steady_clock::now();
-		auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-		std::cout << elapsed_ms << " BTree" << endl;
-
-		begin = std::chrono::steady_clock::now();
-		for (int i = 0; i < size; i++) {
-			EXPECT_TRUE(s.contains(values[i]));
-		}
-		end = std::chrono::steady_clock::now();
-		elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-		std::cout << elapsed_ms << " Set" << endl;
-
-		begin = std::chrono::steady_clock::now();
-		for (int i = 0; i < size; i++) {
-			EXPECT_TRUE(std::find(v.begin(), v.end(), values[i]) != v.end());
-		}
-		end = std::chrono::steady_clock::now();
-		elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-		std::cout << elapsed_ms << " Vector" << endl;
 	}
 }
